b6a9fb4b247ce80ec0b86d9cc32ee4d7328ad60b
Paul Haygood <paulthaygood@Pauls-MacBook-Air.local>
2017-07-21 13:20:29 -0400

    finished

--- script.js
+++ script.js
@@ -1,72 +1,128 @@
+
 class Factory{
-  constructor(make, location, airbags, abs, warranty) {
-    this.make = "Mazda";
-    this.location = "USA";
+  constructor(){
+    this.model = 'Mazda';
+    this.location = 'USA';
     this.airbags = true;
     this.abs = true;
-    this.warranty = "60,000 miles / 3 years";
-
-    this.massBuild = function(quantity, options){
-    this.quantity = 100;
-    this.color = "Black";
-    this.model = "Touring CX-5";
+    this.warranty = 60000;
+    this.massBuild = function(){
+      return `Building ${this.quantity} ${this.color} ${this.trim} ${this.model}s`;
     };
-    this.customerBuild = function(color, options){
-    this.quantity = 1;
-    this.color = "Red";
-    this.model = "Model-2"
-    }
+    this.customerBuild = function(){
+      return `Building one ${this.color} ${this.trim} ${this.model} with the following options: ${this.options}`;
+
+    };
+  };
+};
+
+
+class Car extends Factory {
+  constructor(options){
+    super({warranty: options.warranty});
+    this.model = options.model;
+    this.doors = options.doors;
+    this.color = options.color;
+    this.enginetype = options.enginetype;
+    this.transmission = options.transmission;
+    this.trim = options.trim;
+    this.wheelstrim = options.wheelstrim;
+    this.audio = options.audio;
+    this.seatstrim = options.seatstrim;
+    this.moonroof = options.moonroof;
+    this.enginesize = 4;
+    this.navigation = true;
+    this.backupcamera = true;
+    this.warranty = 100000;
   }
 };
 
-  class Car extends Factory {
-  constructor(model, doors, color, engineType, transmission, trim, wheelsTrim, audio, seatsTrim, moonRoof) {
-    super();
-    this.engineSize = 4;
-    this.navigation = true;
-    this.backupCamera = true;
-    this.warranty = "100,000 miles / 5 years";
-  }
-}
 
 class Sport extends Car {
-constructor(model, transmission, trim, wheelsTrim, audio, seatsTrim, top, color) {
-  super();
-  this.moonroof = false;
-  this.enginetype = "gasoline";
-  this.convertible = true;
-  this.doors = 2;
-}
-}
+  constructor(options){
+    super({model: options.model});
+    this.model = options.model;
+    this.trim = options.trim;
+    this.transmission = options.transmission;
+    this.top = options.top;
+    this.color = options.color;
+    this.seatstrim = options.seatstrim;
+    this.audio = options.audio;
+    this.wheelstrim= options.wheelstrim;
+    this.moonroof = false;
+    this.enginetype = 'gasoline';
+    this.convertible = true;
+    this.doors = 2;
+  }
+};
+
 
 class Truck extends Factory {
-constructor(model, color, engineSize, hitch, bed, navigation) {
-  super();
-  this.backupCamera = true;
-  this.audio = "basic";
-  this.warranty = "150,000 miles / 6 years";
-}
-}
+  constructor(options){
+  super({warranty: options.warranty});
+    this.model = options.model;
+    this.color = options.color;
+    this.enginesize = options.enginesize;
+    this.hitch = options.hitch;
+    this.bed = options.bed;
+    this.navigation = options.navigation;
+    this.backupcamera = true;
+    this.audio = 'basic';
+    this.warranty = 150000;
+  }
+};
 
 let mazda = new Car({
-  model: "Mazda3",
-  color: "Red",
-  engine: "Hybrid",
-  transmission: "Automatic",
-  trim: "Touring",
-  wheels: "Base",
-  audio: "Premium",
-  seats: "Leather",
-  moonRoof: true
+  model: 'Mazda3',
+  color: 'red',
+  enginetype: 'hybrid',
+  transmission: 'automatic',
+  trim: 'Touring',
+  wheels: 'base',
+  audio: 'premium',
+  seats: 'leather',
+  moonroof: true
 });
 
 
-let mazda3 = new Car({
-  make: "Mazda",
-  location: "USA",
-  airbags: true,
-  abs: true,
-  warranty: "100,00 miles / 5 years"
+console.log(mazda);
+
+console.log(mazda.massBuild(mazda.quantity = 35000));
+
+console.log(mazda.customerBuild(mazda.color = 'yellow', mazda.options = 'weather package, satellite radio, rear spoiler'));
+
+
+let miataRf = new Sport({
+  model: 'Miata-RF',
+  trim: 'Grand Touring',
+  transmission: 'manual',
+  top: 'hard top',
+  color: 'red',
+  seats: 'leather',
+  audio: 'premium',
+  wheels: 'premium'
+})
+
+console.log(miataRf);
+
+console.log(miataRf.massBuild(miataRf.quantity = 15000));
+
+console.log(miataRf.customerBuild(miataRf.color = 'black', miataRf.options = 'hidden headlights, sports suspension, leather steering wheel, heated seats, adaptive cruise control'));
+
+
+let trailBlazer = new Truck({
+  model: 'Trail Blazer',
+  color: 'blue',
+  trim: 'Sport',
+  enginesize: 8,
+  hitch: true,
+  bed: 'standard',
+  navigation: true,
+  doors: 2
 });
 
-console.log(mazda3);
+console.log(trailBlazer);
+
+console.log(trailBlazer.massBuild(trailBlazer.quantity = 35000, trailBlazer.trim = 'Sport'));
+
+console.log(trailBlazer.customerBuild(trailBlazer.color = 'red', trailBlazer.options = 'seat warmers, tinted windows, fog lamps'));
